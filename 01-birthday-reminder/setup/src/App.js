f69c01c674480db5b9a898919bc882969641b93f
jasem66 <jasemsarafraz66@gmail.com>
2021-12-01 13:32:30 +0200

    Update App.js

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,15 +2,28 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+const [people, setPeople] = useState(data);
+return(
+  <>
+  <div className="container">
+    <h3> amount {people.length}</h3>
+   <List people={people} />
+  <button className="btn" onClick={()=>setPeople([])}>delete all</button>
+  </div>
+    
+  </>
+)
 
-  const [people, setPeople] = useState(data);
-  return <main>
-    <section className="container">
-     <h3>{people.length} birthday today</h3>
-     <List people={people}/>
-     <button className="btn" onClick={()=>setPeople([])}>clear all</button>
-    </section>
-    </main>;
+ 
 }
 
 export default App;
+ // const [people, setPeople] = useState(data);
+  // return <main>
+  //   <section className="container">
+  //    <h3>{people.length} birthday today</h3>
+  //    <List people={people}/>
+  //    <button className="btn"
+  //     onClick={()=>setPeople([])}>clear all</button>
+  //   </section>
+  //   </main>;
