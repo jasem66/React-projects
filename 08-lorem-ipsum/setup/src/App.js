eb11dae12d6060fac54e02b288a96edebd0c5074
jasem66 <jasemsarafraz66@gmail.com>
2022-07-17 20:11:44 +0300

    changes

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
-import data from './data';
+import React from 'react'
+import { useState } from 'react';
+import data from './data'
 function App() {
-  const [count,setCount] = useState(0);
-  const [text,setText] = useState([]);
-   const handleSubmit =(e)=>{
-     e.preventDefault();
-     let amount = parseInt(count);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([])
 
-
-if(count <= 0){
-  amount = 1;
-
-  } 
-   if(count > 8){
-    amount = 8
+  const handleSubmit=(e)=>{
+e.preventDefault()
+let amount = parseInt(count);
+if(count<=0){
+ amount =1
 }
-setText(data.slice(0,amount));
-   }
-  
+if(count>8){
+  amount=8
+}
+setText(data.slice(0,amount))
+console.log(typeof(amount));
+  }
+
   return (
-  <section className='section-center'>
-    <h3>tired of lorem ipsum</h3>
-    <form  className="lorem-form" onSubmit={handleSubmit}>
-      <label htmlFor="amount">paragraphs :</label>
-      <input type="number" name='amount'
-       id='amount' value={count} 
-       onChange={(e)=>setCount(e.target.value)} />
-       <button type='submit' className='btn'>generate</button>
-    </form>
-    <article className="lorem-text">
-    {text.map((item,index)=>{
-      return <p >{item}</p>
-    })}
-    </article>
-  </section>
-    )
+    <div className='section-center'>
+      <h3>Lorem Ipsum</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragraphs :</label>
+
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(s) => setCount(s.target.value)}
+        />
+        <button className='btn' type='submit'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item,index)=>{
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </div>
+  )
 }
 
-export default App;
+export default App
